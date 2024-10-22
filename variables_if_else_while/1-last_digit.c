5376a2f1e91d89259aba8f2e66f26f49510b46b5
Amedjnoun <A.medjnoun@gmail.com>
2024-10-22 22:57:45 +0200

    correcting last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,12 +11,13 @@
 int main(void)
 {
 
-
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
 	last_digit = n % 10;
 
 	printf("Last digit of %d is %d ", n, last_digit);
