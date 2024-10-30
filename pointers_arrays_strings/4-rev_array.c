5e61a16a9cf3abe3bbcff94a70b9c4f5b09fb274
Amedjnoun <A.medjnoun@gmail.com>
2024-10-30 15:38:14 +0100

    3/4/5

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,21 @@
 #include "main.h"
 
+/**
+ * rev_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
+ */
 
+void rev_array(int *a, int n)
+{
+	int i, temp;
+
+	i = 0;
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+		i++;
+	}
 }
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,27 @@
 #include "main.h"
 
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
 
+void rev_string(char *s)
+{
+	int end, start;
+	char temp;
+
+	i = 0;
+	while (s[end] != '\0')
+		{end++;
+		}
+	end--;
+	start = 0;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		end--;
+		start++;
+	}
 }
