8f5c79c4cf4781a7af1fd1e908f2959e1ae6dbbd
Amedjnoun <A.medjnoun@gmail.com>
2024-10-30 14:39:06 +0100

    1-2-3

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,19 @@
-#include "main.h"
+#include "string.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
 
 
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*str != '\0') {
+		len++;
+		s++;
+	}
+	return len;
 }
+
