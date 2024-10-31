06f35ad74e217028487192e3f142efa6bc16e351
Amedjnoun <A.medjnoun@gmail.com>
2024-10-31 09:42:53 +0100

    6-7-8-9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,23 @@
 #include "main.h"
 
+/**
+ * _strcpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the resulting string dest
+ */
 
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
