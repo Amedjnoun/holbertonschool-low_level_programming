488b117616a0fae4ba70c61c0ac6f130af91fbba
Amedjnoun <A.medjnoun@gmail.com>
2024-11-08 11:23:27 +0100

    Project 3 of the week

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -5,12 +5,12 @@
  * main - adds positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: 0
+ * * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
+
 	int i, sum = 0;
 
 	for (i = 1; i < argc; i++)
