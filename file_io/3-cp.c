df7c722ef7ad482d26b7c0e0b8f27c7aba074ec8
Ali <a.medjnoun@gmail.com>
2024-12-08 18:03:42 +0100

    correction 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "main.h"
+#include "file_utils.h"
 
 /**
  * main - Copies the content of a file to another file
@@ -11,59 +12,16 @@
  *
  * Return: 0 on success, 97, 98, 99, or 100 on failure
  */
-
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, read_from, wrote_to;
-	char buffer[1024];
+	int file_from, file_to;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((read_from = read(file_from, buffer, 1024)) > 0)
-	{
-		wrote_to = write(file_to, buffer, read_from);
-		if (wrote_to != read_from)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-
-	if (read_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+	check_args(argc);
+	file_from = open_file_from(argv[1]);
+	file_to = open_file_to(argv[2]);
+	copy_content(file_from, file_to);
+	close_file(file_from);
+	close_file(file_to);
 
 	return (0);
 }
