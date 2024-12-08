edee7ae8d46cb266f0d2e1cde0f34af530654449
Ali <a.medjnoun@gmail.com>
2024-12-08 16:48:18 +0100

    Doubly linked lists

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -18,11 +18,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 /* Function prototypes */
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
