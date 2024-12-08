649fd192c551f25466826545f13659c6e8e77531
Ali <a.medjnoun@gmail.com>
2024-12-08 17:02:37 +0100

    correcting typo in 0-print

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
@@ -18,7 +18,7 @@ size_t print_dlistint(const dlistint_t *h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++; be
+		count++;
 	}
 	return (count);
 }
